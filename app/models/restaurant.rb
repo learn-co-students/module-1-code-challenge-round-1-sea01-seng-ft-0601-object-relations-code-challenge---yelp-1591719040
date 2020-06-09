8926873cf8d07e71dd87e5d0ea908c8baf1cebe8
bbtrombone <bbtrombone@sbcglobal.net>
2020-06-09 10:55:34 -0700

    steven chase

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,12 @@ class Restaurant
     @name = name
   end
 
+  def name
+    @name 
+  end
+
+  def reviews
+    Review.all
+  end 
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,27 @@
 class Review
-  
+
+    attr_reader :customer, :restaurant, :rating
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating =  rating
+        @@all << self
+    end 
+
+    def customer
+        @customer
+    end 
+
+    def restaurant
+        @restaurant
+    end 
+
+    def rating
+        @rating
+    end 
+
+    def self.all
+    end 
 end
