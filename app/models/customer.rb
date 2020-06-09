5ec6794ea630c94e2cebac3d248ac52d198b7b36
dbpatnodee <dbpatnode@hotmail.com>
2020-06-09 11:05:18 -0700

    could use some work

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,24 @@
 class Customer
   attr_accessor :given_name, :family_name
-
+  
+  @@all= []
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  # Returns a **unique** array of all restaurants a customer has reviewed
+  def restaurants
+      Review.all.select { |review| review.resturaunt == self }.uniq
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,18 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+daniel_patnode = Customer.new("Daniel", "Patnode")
+john_smith = Customer.new("John", "Smith")
+jon_doe = Customer.new("John", "Doe")
+
+mcdonalds = Restaurant.new("McDonalds")
+wendys= Restaurant.new("Wendys")
+jack_in_the_box = Restaurant.new("Jack in the Box")
+
+r1 = Review.new(daniel_patnode, wendys, 6)
+r2 = Review.new(daniel_patnode, mcdonalds, 7)
+r3 = Review.new(john_smith, jack_in_the_box, 5)
+r4 = Review.new(jon_doe, mcdonalds, 8)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
