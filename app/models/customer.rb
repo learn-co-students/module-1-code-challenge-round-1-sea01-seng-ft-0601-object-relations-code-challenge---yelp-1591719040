3ab91dc0e7249d01d05c590b6933237abeefb34c
ronakvsoni <ronakvsoni@gmail.com>
2020-06-09 11:35:20 -0700

    codechallenge 3

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,14 @@
 class Customer
   attr_accessor :given_name, :family_name
-
+@@all = []
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
+
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review
-  
+  def initialize(customer,restaurant,rating)
+@customer = customer
+@restaurant = restaurant
+@rating = rating
+@@all = []
 end
+
+
+
