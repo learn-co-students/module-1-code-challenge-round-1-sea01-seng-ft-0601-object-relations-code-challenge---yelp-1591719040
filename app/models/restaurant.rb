442c149ded18173399d3ed68051c44c70ad11168
cortezd334 <cortezd334@gmail.com>
2020-06-09 10:53:13 -0700

    First CC

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,25 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def reviews
+  #binding.pry
+    Review.all.find_all {|reviews| if reviews.restaurant == self
+      reviews.rating end}
+  end
+
+  def customers
+     binding.pry
+    cust = self.reviews.collect {|customers| customers.customer}
+  #list of cust name instead of array?
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,27 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+#restaurant
+fogon = Restaurant.new("Fogon")
+ruth_chris = Restaurant.new("Ruth Chris")
+sushi_ka = Restaurant.new("Sushi Ka")
+
+#customer
+danira = Customer.new("Danira", "Cortez")
+erika = Customer.new("Erika", "Cortez")
+david = Customer.new("David", "Cortez")
+kenz = Customer.new("Kenz", "Brown")
+em = Customer.new("Emily", "Paul")
+
+#review
+r1 = Review.new(danira, fogon, 5)
+r2 = Review.new(erika, fogon, 4)
+r3 = Review.new(david, ruth_chris, 3)
+r4 = Review.new(em, ruth_chris, 5)
+r5 = Review.new(kenz, sushi_ka, 1)
+r6 = Review.new(danira, sushi_ka, 4)
+r7 = Review.new(erika, ruth_chris, 2)
+r8 = Review.new(kenz, sushi_ka, 4)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
